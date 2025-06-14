4fc2149564b80cef555e99d825bc6a2b1dbabaec
Watic <33091164+watic-automacao@users.noreply.github.com>
2025-06-14 20:19:30 -0300

    Update config.php
    adicionado credenciais do db

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 /* Configuration of the site */
-define('DATABASE_SERVER',   '');
-define('DATABASE_USERNAME', '');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_NAME',     '');
-define('SITE_URL',          '');
+define('DATABASE_SERVER',   'wordpress_bio-novo-db');
+define('DATABASE_USERNAME', 'bio_admin');
+define('DATABASE_PASSWORD', '84341125');
+define('DATABASE_NAME',     'bio');
+define('SITE_URL',          'https://wordpress-bio-novo.1aa7s0.easypanel.host');
